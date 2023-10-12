9ecc66e8b13ac702afe469f04f70eecd4f2e7c82
Margarita <Margarita>
2023-10-12 20:23:37 +0300

    fix: rename controller and update build command

--- server/src/router/router.ts
+++ server/src/router/router.ts
@@ -1,45 +1,45 @@
 import express from 'express'
 import { check } from 'express-validator'
-import PostController from '../User/controller'
+import UserController from '../User/controller'
 import { ERROR_MESSAGES } from '../const/errorMessages'
 
 const router = express.Router()
 
 const enum Routes {
-  signIn = '/sign_in',
-  signUp = '/sign_up',
-  users = '/users'
+	signIn = '/sign_in',
+	signUp = '/sign_up',
+	users = '/users'
 }
 
 const checkIsEmpty = (key: string) => check(key, `${ERROR_MESSAGES.empty} ${key}`).notEmpty()
 
 const checkEmail = (key: string = 'email') => [
-  check(key, `${ERROR_MESSAGES.invalid} ${key}`).isEmail(),
-  checkIsEmpty(key)
+	check(key, `${ERROR_MESSAGES.invalid} ${key}`).isEmail(),
+	checkIsEmpty(key)
 ]
 
 const checkPassword = () => checkIsEmpty('password')
 
 router.post(
-  Routes.signIn,
-  [
-    ...checkEmail(),
-    checkPassword()
-  ],
-  PostController.signIn
+	Routes.signIn,
+	[
+		...checkEmail(),
+		checkPassword()
+	],
+	UserController.signIn
 )
 router.post(
-  Routes.signUp,
-  [
-    ...checkEmail(),
-    checkPassword(),
-    checkIsEmpty('username')
-  ],
-  PostController.signUp
+	Routes.signUp,
+	[
+		...checkEmail(),
+		checkPassword(),
+		checkIsEmpty('username')
+	],
+	UserController.signUp
 )
-router.get(Routes.users, PostController.getAll)
-router.get(`${Routes.users}/:id`, PostController.getUserById)
-router.post(Routes.users, PostController.delete)
-router.put(Routes.users, PostController.update)
+router.get(Routes.users, UserController.getAll)
+router.get(`${Routes.users}/:id`, UserController.getUserById)
+router.post(Routes.users, UserController.delete)
+router.put(Routes.users, UserController.update)
 
 export default router
